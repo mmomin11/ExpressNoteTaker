dbbf1a6f53d25c726685ba9d33e48d2476a4b80d
mmomin11 <mubin.momin@nxp.com>
2020-07-10 09:07:19 -0500

    npm install, pseudo initial api requests

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+// require express 
+
+// require path
+
+// require fs to read and write files
+
+// create app const which would be the express server
+
+// initial ports
+
+
+// create a global array for the notes
+
+// api call response for all the ntoes, and sends results to browser as an array of objects. 
+
+// reads the data from json file
+// writes the new note to json file
+// delete a note
+
+
